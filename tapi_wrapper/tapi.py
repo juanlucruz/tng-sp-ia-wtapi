ad8bb4143049d81ac92dc5fac576d5eff7b628f9
srjuanlu <jdelacruz@cttc.es>
2019-06-02 07:59:51 +0200

    fetch sips by name and remove duplicates

--- tapi_wrapper/tapi.py
+++ tapi_wrapper/tapi.py
@@ -159,7 +159,11 @@ class TapiWrapper(object):
             sip_inv = self.engine.get_sip_inventory(':'.join([wim[2], '8182']))
             vim_inv = self.get_vims_setup()
             old_endpoints = self.clean_wim_old_attachments(wim[0])
-            self.clean_endpoints_from_vim_db(old_endpoints)
+            sip_name_list = (
+                name['value'] for sip in sip_inv for name in sip['name']
+                if name['value-name'] == 'public-name'
+            )
+            self.clean_endpoints_from_vim_db(old_endpoints, sip_name_list)
             for sip in sip_inv:
                 LOG.debug(f'Processing sip {sip}')
                 vim_match = self.check_sip_vim(sip, vim_inv)
@@ -268,8 +272,7 @@ class TapiWrapper(object):
             if connection:
                 connection.close()
 
-
-    def clean_endpoints_from_vim_db(self, db_endpoints):
+    def clean_endpoints_from_vim_db(self, db_endpoints, sip_names=()):
         connection = None
         cursor = None
         try:
@@ -284,6 +287,18 @@ class TapiWrapper(object):
             LOG.debug(f'query_delete: {query_delete}')
             cursor.execute(query_delete)
             connection.commit()
+            # GET VIM endpoints
+            if sip_names:
+                query_names = f"SELECT uuid FROM vim WHERE vendor = 'endpoint' AND name in {sip_names}"
+                LOG.debug(f'query_names: {query_names}')
+                cursor.execute(query_names)
+                db_endpoints_by_name = cursor.fetchall()
+                LOG.debug(f"Removing {db_endpoints_by_name} from vimregistry to avoid duplicates")
+                query_delete_by_name = f"DELETE FROM vim WHERE vendor = 'endpoint' AND uuid in {db_endpoints_by_name}"
+                LOG.debug(f'query_delete_by_name: {query_delete_by_name}')
+                cursor.execute(query_delete_by_name)
+                connection.commit()
+                db_endpoints.append(db_endpoints_by_name)
             return db_endpoints
         except (Exception, psycopg2.Error) as error:
             LOG.error(error)
