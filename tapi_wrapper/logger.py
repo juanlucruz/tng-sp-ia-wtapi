46a47fce85c3d63b58e41ec33334fce8801c9bbd
srjuanlu <jdelacruz@cttc.es>
2019-06-21 15:03:55 +0200

    test logging timestamp

--- tapi_wrapper/logger.py
+++ tapi_wrapper/logger.py
@@ -147,7 +147,7 @@ class TangoJsonLogHandler(logging.StreamHandler):
         d = {
             # TANGO default fields
             "type": record.levelname[0],
-            "timestamp": "{} UTC".format(record.asctime),
+            "timestamp": "{} UTC".format(record.__dict__.get("asctime", datetime.datetime.utcnow())),
             "start_stop": record.__dict__.get("start_stop", ""),
             "component": record.name,
             "operation": record.__dict__.get("operation", record.funcName),
